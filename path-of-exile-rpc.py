78c819835e728a107cb11569b56a336181af60ca
Stefankar1000 <stefankar1000@gmail.com>
2020-03-13 16:53:00 +0100

    Clean up the main script.
    No new features, just cleaned everything up.

--- path-of-exile-rpc.py
+++ path-of-exile-rpc.py
@@ -2,64 +2,64 @@ import sys
 import time
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+
 from pypresence import Presence
 
-RPC = Presence("655060628582432768")
 
-def update_presence(*, state: str = None, details: str = None):
-    RPC.update(large_image="logo",
-               small_image="rem-padoru",
-               large_text="Conquerors of the Atlas",
-               small_text="Rich Presence by @Predator#4682",
-               start=begin_time,
-               state=state)
+def set_presence(state: str = None):
+    presence.update(large_image="logo",
+                    large_text="git.io/Jv6NZ",
+                    start=begin_time,
+                    state=state)
 
-class CustomFileEventHandler(FileSystemEventHandler):
+
+class FileEventHandler(FileSystemEventHandler):
     def on_modified(self, event):
         file_path = event.src_path
         # Only watch for the Client.txt log file
         if "Client.txt" not in file_path:
             return
-        
+
         with open(file_path, "r") as f:
+            # Read the last line from Client.txt
             line = f.readlines()[-1]
 
-            if "Connected to" in line:
-                connected_server = line.split("Connected to")[1].strip().split(" ")[0]
-                connected_time = line.split("Connected to")[-1].strip().split(" ")[-1].replace("ms.", "")
-                print(f"Connected to server {connected_server} in {connected_time}ms.")
-                
-                update_presence(state="Idling")
+            if "entered" in line:
+                # Strip the line to just the location name
+                entered_location = \
+                    line.split("entered")[1].replace(".", "").strip()
 
+                set_presence(entered_location)
+                print(f"Entered {entered_location}")
 
-            if "You have entered" in line:
-                entered_location = line.split("You have entered")[1].strip().replace(".", "")
-                print(f"Entering {entered_location}.")
-
-                update_presence(state=entered_location)
-            
-path = sys.argv[1]
-event_handler = CustomFileEventHandler()
-observer = Observer()
 
 if __name__ == "__main__":
+    path = sys.argv[1]
+    if not path:
+        print("Log path not given, exitting.")
+        exit(1)
+
     begin_time = time.time()
+    presence = Presence("655060628582432768")
+    observer = Observer()
+    event_handler = FileEventHandler()
 
     observer.schedule(event_handler, path)
     observer.start()
-    print("Started watcher")
+    print("Started file watcher")
 
-    RPC.connect()
-    print("Started presence")
+    presence.connect()
+    print("Started Discord Presence")
+    print("=" * 20, end="\n\n")
 
     # Default presence
-    update_presence(state="Idling")
+    set_presence("Main Menu")
 
     try:
         while True:
             time.sleep(1)
     except KeyboardInterrupt:
         observer.stop()
-        RPC.close()
+        presence.close()
 
     observer.join()
